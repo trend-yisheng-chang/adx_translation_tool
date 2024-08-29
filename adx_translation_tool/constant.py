dd0c40fbc90c7348dab1df391339555d032dec4c
Eason <eason4best@gmail.com>
2024-08-29 11:08:51 +0800

    Include kusto_keywords.txt

--- adx_translation_tool/constant.py
+++ adx_translation_tool/constant.py
@@ -3,3 +3,5 @@ import os
 OPEN_AI_BASE_URL = 'https://api.rdsec.trendmicro.com/prod/aiendpoint/v1/'
 GROUND_TRUTH_JSON_PATH = os.path.join(
     os.path.dirname(__file__), 'ground_truths.json')
+KUSTO_KEYWORD_TXT_PATH = os.path.join(
+    os.path.dirname(__file__), 'kusto_keywords.txt')
