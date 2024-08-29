acc9fcb554d0942bc0c035acc50f4f15df41e018
Eason <eason4best@gmail.com>
2024-08-29 10:35:23 +0800

    Update constant.py

--- adx_translation_tool/constant.py
+++ adx_translation_tool/constant.py
@@ -1,2 +1,5 @@
-OPEN_AI_BASE_URL = ''
-GROUND_TRUTH_JSON_PATH = ''
+import os
+
+OPEN_AI_BASE_URL = 'https://api.rdsec.trendmicro.com/prod/aiendpoint/v1/'
+GROUND_TRUTH_JSON_PATH = os.path.join(
+    os.path.dirname(__file__), 'ground_truths.json')
